86c4ab0fd9c313031361c7bdd3425006915dae56
Miihh <69526529+Miihh@users.noreply.github.com>
2023-04-24 15:43:32 +0300

    Add files via upload
    first commit on PlantCare

---
+++ plantcare.js
@@ -0,0 +1,25 @@
+function test(nume, prenume) {
+
+    console.log("Numele meu este" +" " + prenume + " " + nume + "!");
+}
+
+test("Blagniceanu", "Mihai");
+
+
+
+
+let loginEmail = document.getElementById("InputEmail1");
+let loginPassword = document.getElementById("InputPassword1");
+
+
+
+function login () {
+    let email = loginEmail.value;
+    let password = loginPassword.value;
+    console.log("you have loged in" + " " + email + " " + password);
+    
+}
+
+function showSignUp() {
+ 
+}
